83413f1935e1dbf3c8d0bc77b7d0a5d45f4738d9
aakashstha1 <aakash.cerestha90@gmail.com>
2025-05-27 15:18:26 +0545

    first commit

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import Layout from "@/layouts/Layout";
+import Home from "@/pages/Home";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
